a150804ea9dbd7b73de15ac1a64ad59a7b59944c
TheLuisAbarca <arclaav.adm@gmail.com>
2022-09-28 17:17:40 -0500

    Define basic types using typescript syntax.

--- src/typescript/BasicTypes.tsx
+++ src/typescript/BasicTypes.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 export const BasicTypes = () => {
+    const name: string = 'Luis';
+    const edad: number  = 35;
+    const isActive: boolean = true;
+
+    const powers: string[] = ['Velocity', 'Fly', 'Breath under water'];
+
+
   return (
     <>
         <h3>Basic Types</h3>
+        { name }, { edad }, { (isActive) ? 'Active' : 'Inactive' }
+        <br/>
+        { powers.join(', ') }
     </>
   )
 }
