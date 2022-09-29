e627eb0d3aeb31c600c9a8a22cdde983fa31fa3f
TheLuisAbarca <arclaav.adm@gmail.com>
2022-09-29 10:42:35 -0500

    Add more logic to the authreducer: action.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,62 @@
+import { useEffect, useReducer } from "react"
+
+interface AuthState {
+    validating: boolean,
+    token: string | null,
+    username: string,
+    name: string,
+}
+
+const initialState:AuthState ={
+    validating: true,
+    token: null,
+    username: '',
+    name: ''
+};
+
+type AuthAction = { type: 'logout' };
+
+const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+    switch (action.type) {
+        case 'logout':
+            return {
+                validating: false,
+                token: null,
+                username: '',
+                name: ''
+            };
+            
+    
+        default:
+            return state;
+    }
+}
 
 export const Login = () => {
+
+    const [{validating}, dispatch] = useReducer(authReducer, initialState);
+    useEffect(() => {
+      setTimeout(() => {
+        dispatch({ type: 'logout'})
+      }, 1500);
+    
+    }, []);
+
+    if ( validating ) {
+        return (
+            <>
+                <h3>Login</h3>
+                <div className="alert alert-info">
+                    Validating...
+                </div>
+            </>
+        )
+    }
+    
+
   return (
     <>
         <h3>Login</h3>
-
-        <div className="alert alert-info">
-            Validating...
-        </div>
 
         <div className="alert alert-danger">
             Not Authenticated!
