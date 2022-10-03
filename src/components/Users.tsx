34fb6ae5074cb80b9fecea6ffe81e1a3812867f9
TheLuisAbarca <arclaav.adm@gmail.com>
2022-10-03 09:28:54 -0500

    Create a little pagination.

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,19 +1,38 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { reqResApi } from "../api/reqRes";
 import { ReqResList, User } from "../interfaces/reqRes";
 
 export const Users = () => {
 
     const [users, setUser] = useState<User[]>([]);
+    const pageRef = useRef(1);
 
     useEffect(() => {
-        reqResApi.get<ReqResList>('/users')
+        loadUsers();
+    }, []);
+
+    const loadUsers = async() => {
+        const answer = await reqResApi.get<ReqResList>('/users', {
+            params: {
+                page: pageRef.current
+            }
+        });
+
+        if( answer.data.data.length > 0){
+            setUser( answer.data.data );
+            pageRef.current ++;
+        } else {
+            alert("There is no more registries or pages.");
+        }
+        setUser( answer.data.data );
+        /*
         .then( resp => {
             console.log(resp.data.data[0].last_name);
             setUser(resp.data.data);
         })
         .catch( console.log );
-    }, []);
+        */
+    }
 
     const renderItem = ( {id, first_name, last_name, email, avatar}: User ) => {
         return (
@@ -52,7 +71,8 @@ export const Users = () => {
             </tbody>
         </table>
         <button
-            
+            className="btn btn-primary"
+            onClick={ loadUsers }
         >
             Next 
         </button>
