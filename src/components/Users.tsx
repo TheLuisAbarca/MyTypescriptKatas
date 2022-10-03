0248ec3b5a653dd18cc396d8029ae60754972bc6
TheLuisAbarca <arclaav.adm@gmail.com>
2022-10-03 09:01:26 -0500

    Add types and function to render Items.

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,16 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { reqResApi } from "../api/reqRes";
+import { ReqResList, User } from "../interfaces/reqRes";
 
 export const Users = () => {
 
+    const [users, setUser] = useState<User[]>([]);
+
     useEffect(() => {
-        reqResApi.get('/users')
+        reqResApi.get<ReqResList>('/users')
         .then( resp => {
-            console.log(resp.data.data);
+            console.log(resp.data.data[0].last_name);
+            setUser(resp.data.data);
         })
         .catch( console.log );
     }, []);
-    
+
+    const renderItem = ( {id, first_name, last_name, email, avatar}: User ) => {
+        return (
+            <tr key={ id.toString() }>
+                <td>
+                    <img 
+                        src={ avatar }
+                        alt={ first_name }
+                        style={{
+                            width: 35,
+                            borderRadius: 100
+                        }}
+                    />
+                </td>
+                <td>{first_name} {last_name}</td>
+                <td>{email}</td>
+            </tr>
+        );
+    }
 
   return (
     <>
@@ -24,9 +46,16 @@ export const Users = () => {
                 </tr>
             </thead>
             <tbody>
-                
+                {
+                    users.map( renderItem )
+                }
             </tbody>
         </table>
+        <button
+            
+        >
+            Next 
+        </button>
     </>
   )
 }
